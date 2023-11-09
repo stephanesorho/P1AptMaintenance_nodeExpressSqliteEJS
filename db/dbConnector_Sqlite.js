17f6b524d145a057704beac54860fa006451e0b6
Stephane Sorho <sfsorho@gmail.com>
2023-11-08 22:24:03 -0800

    added base files, edit + create pages

--- db/dbConnector_Sqlite.js
+++ db/dbConnector_Sqlite.js
@@ -10,64 +10,125 @@ async function connect() {
 
 async function getUsers() {
   const db = await connect();
-  const users = await db.all(`SELECT * FROM User`);
 
-  console.log("dbConnector got users", users);
-  return users;
+  try {
+    const users = await db.all(`SELECT * FROM User`);
+
+    console.log("dbConnector got users", users);
+    return users;
+  } finally {
+    await db.close();
+  }
+}
+
+async function getUserById(user_id) {
+  const db = await connect();
+
+  try {
+    const stmt = await db.prepare(`SELECT *
+    FROM User
+    WHERE
+      user_id = :user_id
+  `);
+
+    stmt.bind({
+      ":user_id": user_id,
+    });
+
+    const user = await stmt.all();
+
+    await stmt.finalize();
+
+    return user;
+  } finally {
+    await db.close();
+  }
+}
+
+async function updateUser(user_id, newUser) {
+  console.log("update user user_id ", user_id);
+  const db = await connect();
+
+  try {
+    const stmt = await db.prepare(`UPDATE User
+    SET
+      name = :name,
+      apt_number = :apt_number,
+      email = :email
+    WHERE
+      user_id = :user_id
+  `);
+
+    stmt.bind({
+      ":user_id": user_id,
+      ":name": newUser.name,
+      ":apt_number": newUser.apt_number,
+      ":email": newUser.email,
+    });
+
+    const result = await stmt.run();
+
+    await stmt.finalize();
+
+    return result;
+  } finally {
+    await db.close();
+  }
+}
+
+async function deleteUser(user_id) {
+  console.log("deleting user ", user_id);
+  const db = await connect();
+
+  try {
+    const stmt = await db.prepare(`DELETE FROM User
+    WHERE
+      user_id = :user_id
+  `);
+
+    stmt.bind({
+      ":user_id": user_id,
+    });
+
+    const result = await stmt.run();
+
+    await stmt.finalize();
+
+    return result;
+  } finally {
+    await db.close();
+  }
 }
 
 async function createUser(newUser) {
   const db = await connect();
 
-  const stmt = await db.prepare(`INSERT INTO
-    User(user_id, name, apt_number, email)
-    VALUES (:user_id, :name, :apt_number, :email)
+  try {
+    const stmt = await db.prepare(`INSERT INTO
+    User(name, apt_number, email)
+    VALUES (:name, :apt_number, :email)
   `);
 
-  stmt.bind({
-    ":user_id": newUser.user_id,
-    ":name": newUser.name,
-    ":apt_number": newUser.apt_number,
-    ":email": newUser.email,
-  });
+    stmt.bind({
+      ":name": newUser.name,
+      ":apt_number": newUser.apt_number,
+      ":email": newUser.email,
+    });
 
-  return await stmt.run();
-}
+    const result = await stmt.run();
 
-async function getUserByID(user_id) {
-  const db = await connect();
+    await stmt.finalize();
 
-  const stmt = await db.prepare(`SELECT *
-    FROM User
-    WHERE
-      user_id == user_id
-  `);
-
-  stmt.bind({
-    ":user_id": user_id,
-  });
-
-  return await stmt.get();
-}
-
-async function deleteUser(userToDelete) {
-  const db = await connect();
-
-  const stmt = await db.prepare(`DELETE FROM
-    User
-    WHERE user_id == theIDToDelete
-  `);
-
-  stmt.bind({
-    ":theIDToDelete": userToDelete.user_id,
-  });
-
-  return await stmt.run();
+    return result;
+  } finally {
+    await db.close();
+  }
 }
 
 module.exports = {
   getUsers,
-  getUserByID,
-  createUser,
+  getUserById,
+  updateUser,
   deleteUser,
+  createUser,
 };
--- routes/index.js
+++ routes/index.js
@@ -1,59 +1,163 @@
 let express = require("express");
+const { redirect } = require("express/lib/response");
 let router = express.Router();
 
-const myDB = require("../db/dbConnector_Sqlite.js");
+const {
+  getUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
+  createUser,
+} = require("../db/dbConnector_Sqlite.js");
 
 /* GET home page. */
 router.get("/", async function (req, res) {
-  console.log("Got request for /");
-  const users = await myDB.getUsers();
-
-  res.render("index", { users: users });
+  try {
+    console.log("Got request for /");
+    const users = await getUsers();
+    res.render("index", {
+      users,
+      err: "",
+      type: "success",
+    });
+  } catch (exception) {
+    console.log("Error execution SQL", exception);
+    res.render("index", {
+      title: "Apartment Maintenance App",
+      users: [],
+      err: `Error execution SQL ${exception}`,
+      type: "danger",
+    });
+  }
 });
 
-/* GET user ID. */
-router.get("/users/:user_id", async function (req, res) {
-  console.log("Got user id");
+/* Render Edit page. */
+router.get("/users/:user_id/edit", async function (req, res) {
+  console.log("Edit route", req.params.user_id);
+  try {
+    const sqlRes = await getUserById(req.params.user_id);
+    console.log("users edit found user", sqlRes);
+    if (sqlRes.length === 1) {
+      res.render("users_edit", { user: sqlRes[0], err: null, type: "success" });
+    } else {
+      res.render("users_edit", {
+        user: null,
+        err: "Error finding User " + req.params.user_id,
+        type: "danger",
+      });
+    }
+  } catch (exception) {
+    console.log("Error executing SQL", exception);
+    res.render("index", {
+      user: null,
+      err: `Error executing SQL ${exception}`,
+      type: "danger",
+    });
+  }
+});
+
+// Actually edits a User in table
+router.post("/users/:user_id/edit", async function (req, res) {
+  console.log("Edit route", req.params.user_id, req.body);
 
   const user_id = req.params.user_id;
+  const newUser = req.body;
 
-  console.log("got user id ", user_id);
+  try {
+    const sqlResUpdate = await updateUser(user_id, newUser);
+    console.log("Updating user", sqlResUpdate);
 
-  const userID = await myDB.getUserByID(user_id);
-
-  console.log("User created");
-
-  res.render("User ID", { userID });
+    if (sqlResUpdate.changes === 1) {
+      const sqlResFind = await getUserById(req.params.user_id);
+      res.render("users_edit", {
+        user: sqlResFind[0],
+        err: "User modified",
+        type: "success",
+      });
+    } else {
+      res.render("users_edit", {
+        user: null,
+        err: "Error Updating User " + req.params.user_id,
+        type: "danger",
+      });
+    }
+  } catch (exception) {
+    console.log("Error executing SQL", exception);
+    res.render("index", {
+      user: null,
+      err: `Error executing SQL ${exception}`,
+      type: "danger",
+    });
+  }
 });
 
-/* POST create user. */
+// Delete a user
+router.get("/users/:user_id/delete", async function (req, res) {
+  console.log("Delete route", req.params.user_id);
+
+  try {
+    const sqlResDelete = await deleteUser(req.params.user_id);
+    console.log("Delete user res=", sqlResDelete);
+    const users = await getUsers();
+
+    if (sqlResDelete.changes === 1) {
+      res.render("index", { users, err: "User deleted", type: "success" });
+    } else {
+      res.render("index", {
+        users,
+        err: "Error deleting user",
+        type: "danger",
+      });
+    }
+  } catch (exception) {
+    console.log("Error executing SQL", exception);
+    res.render("index", {
+      users,
+      err: "Error executing SQL",
+      type: "danger",
+    });
+  }
+});
+
+// Render Create page
+router.get("/users/create", async function (req, res) {
+  console.log("Create route", req.params.user_id);
+
+  res.render("users_create", { err: null, type: "success" });
+});
+
+// Actually creates User
 router.post("/users/create", async function (req, res) {
-  console.log("Got post create/users");
+  console.log("Create route", req.body);
 
-  const user = req.body;
+  const newUser = req.body;
 
-  console.log("got create user", user);
+  try {
+    const sqlResCreate = await createUser(newUser);
+    console.log("Updating user", sqlResCreate);
+    const users = await getUsers();
 
-  await myDB.createUser(user);
-
-  console.log("User created");
-
-  res.redirect("/");
-});
-
-/* POST delete user. */
-router.post("/users/delete", async function (req, res) {
-  console.log("Got post delete user");
-
-  const user = req.body;
-
-  console.log("got delete user", user);
-
-  await myDB.deleteUser(user);
-
-  console.log("User deleted");
-
-  res.redirect("/");
+    if (sqlResCreate.changes === 1) {
+      res.redirect("/");
+      res.render("index", {
+        users,
+        err: "User Created " + sqlResCreate.lastID,
+        type: "success",
+      });
+    } else {
+      res.render("user_create", {
+        err: "Error inserting user",
+        type: "danger",
+      });
+    }
+  } catch (exception) {
+    console.log("Error executing SQL", exception);
+    res.render("index", {
+      users: null,
+      err: "Error inserting user" + exception,
+      type: "danger",
+    });
+  }
 });
 
 module.exports = router;
